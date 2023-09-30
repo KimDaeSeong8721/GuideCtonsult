7a0dc6d9b1c106877eacb98f9b2b1c78ea882e1b
KimDaeSeong8721 <KimDaeSeong8721@github.com>
2023-09-30 11:22:31 +0900

    [ADD] 예약화면 UI

--- GuideConsult/ContentView.swift
+++ GuideConsult/ContentView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        GuideListView()
-    }
+     
+        TabView {
+            GuideListView()
+            .tabItem {
+                Image(systemName: "magnifyingglass")
+                Text("")
+              }
+              .background()
+            Text("Another Tab")
+              .tabItem {
+                Image(systemName: "book.fill")
+                Text("")
+              }
+            Text("The Last Tab")
+              .tabItem {
+                Image(systemName: "gear")
+                Text("")
+              }
+              .badge(10)
+          }
+        .tint(.point)
+        }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        StartView()
+        ContentView()
     }
 }
--- GuideConsult/Screen/Reservation/ReservationView.swift
+++ GuideConsult/Screen/Reservation/ReservationView.swift
@@ -12,9 +12,19 @@ struct ReservationView: View {
     //MARK: - Properties
     @State private var date = Date()
     
+    private var endTime: Date {
+        return Date(timeInterval: 60*60, since: date)
+    }
+    
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateFormat = "yy.MM.dd"
+        return formatter
+    }()
+    
+    private let timeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm a"
         return formatter
     }()
     
@@ -22,95 +32,117 @@ struct ReservationView: View {
     var body: some View {
         GeometryReader { geo in
             VStack {
-                ScrollView {
-                    VStack {
-                        //MARK: - 프로필 정보
-                        BoxView(padding: 16) {
-                            HStack {
-                                Image("profile")
-                                    .resizable()
-                                    .frame(width: 114, height: 114)
-                                VStack(alignment: .leading, spacing: 5) {
-                                    HStack {
-                                        Text("DAESEONG KIM")
-                                            .font(.system(size: 16, weight: .semibold))
-                                        Image("koreaFlag")
-                                            .resizable()
-                                            .frame(width: 15, height: 15)
-                                    }
-                                    
-                                    HStack(spacing: 5) {
-                                        Image(systemName: "globe")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        Text("English & Korean")
-                                    }
-                                    
-                                    HStack(spacing: 5) {
-                                        Image(systemName: "house")
-                                            .resizable()
-                                            .frame(width: 12, height: 10)
-                                        Text("Seoul Dobongu")
-                                    }
-                                    
-                                    HStack(spacing: 5) {
-                                        Image("map")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        Text("English & KoreanDobongu")
-                                            .fixedSize(horizontal:false, vertical: true)
-                                    }
-                                    
-                                    HStack(spacing: 5) {
-                                        Image("Coffee")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        Text("Dutch pay")
-                                    }
-                                    
-                                    HStack(spacing: 5) {
-                                        Image("Instar")
-                                            .resizable()
-                                            .frame(width: 12, height: 12)
-                                        Text("********")
-                                            .opacity(0.4)
-                                    }
-                                    
+                Form {
+                    //MARK: - 프로필
+                    Section {
+                        HStack {
+                            Image("profile")
+                                .resizable()
+                                .frame(width: 114, height: 114)
+                            VStack(alignment: .leading, spacing: 5) {
+                                HStack {
+                                    Text("DAESEONG KIM")
+                                        .font(.system(size: 16, weight: .semibold))
+                                    Image("koreaFlag")
+                                        .resizable()
+                                        .frame(width: 15, height: 15)
                                 }
-                                .font(.system(size: 12))
+                                
+                                HStack(spacing: 5) {
+                                    Image(systemName: "globe")
+                                        .resizable()
+                                        .frame(width: 12, height: 12)
+                                    Text("English & Korean")
+                                }
+                                
+                                HStack(spacing: 5) {
+                                    Image(systemName: "house")
+                                        .resizable()
+                                        .frame(width: 12, height: 10)
+                                    Text("Seoul Dobongu")
+                                }
+                                
+                                HStack(spacing: 5) {
+                                    Image("map")
+                                        .resizable()
+                                        .frame(width: 12, height: 12)
+                                    Text("English & KoreanDobongu")
+                                        .fixedSize(horizontal:false, vertical: true)
+                                }
+                                
+                                HStack(spacing: 5) {
+                                    Image("Coffee")
+                                        .resizable()
+                                        .frame(width: 12, height: 12)
+                                    Text("Dutch pay")
+                                }
+                                
+                                HStack(spacing: 5) {
+                                    Image("Instar")
+                                        .resizable()
+                                        .frame(width: 12, height: 12)
+                                    Text("********")
+                                        .opacity(0.4)
+                                }
+                                
+                            }
+                            .font(.system(size: 12))
+                            Spacer()
+                        }
+                    }
+                    //MARK: - 캘린더
+                    Section {
+                        DatePicker(
+                            "Start Date",
+                            selection: $date,
+                            displayedComponents: [.date])
+                        .datePickerStyle(.graphical)
+                        
+                    } header: {
+                        Text("Reservation Date")
+                            .padding(.leading, -16)
+                            .foregroundColor(.black)
+                    }
+
+                    //MARK: - 시간
+                    Section {
+                        VStack {
+                            DatePicker("Pick a Reservation Time", selection: $date,displayedComponents: [.hourAndMinute])
+                            
+                            Divider()
+                            HStack {
+                                Text("End Time")
                                 Spacer()
+                                Text("\(timeFormatter.string(from: endTime))")
+                                    .font(.system(size: 16))
+                                    .padding(.horizontal,10)
                             }
-                            .frame(maxWidth: .infinity)
                         }
-                        .padding(16)
-                        
-                        
-                        //MARK: - 캘린더
-                        VStack(alignment: .leading, spacing: 5) {
-                            
-                            Text("Reservation Date")
-                                .font(.system(size: 16, weight: .semibold))
-                                .padding(.leading, 8)
-                            
-                            BoxView {
-                                DatePicker(
-                                    "Start Date",
-                                    selection: $date,
-                                    displayedComponents: [.date])
-                                .datePickerStyle(.graphical)
-                            }
-                            .font(.system(size: 14))
-                        }
-                        .padding(.horizontal,16)
-                        .frame(maxWidth: .infinity)
-                        
-                        //MARK: - 시간
-                        
-                        //MARK: - 취소 및 환불규정
-                        
-                        
+                    } header: {
+                        Text("Time")
+                            .padding(.leading, -16)
+                            .foregroundColor(.black)
+                    }
+                    
+                    //MARK: - 취소 환불 규정
+
+                    Section {
+                        Text("""
+        Eligibility for Refunds and Exchanges
+        - Your item must be unused and in the same condition that you received it.
+        - The item must be in the original packaging. /
+        - To complete your return, we require a receipt or proof of purchase.
+        - Only regular priced items may be refunded, sale items cannot be refunded.
+        - If the item in question was marked as a gift when purchased and shipped directly to you, you will receive a gift credit for the value of your return
+        """)
+                     .font(.system(size: 14))
+                    } header: {
+                        Text("Policy for Cancel and Refunds")
+                            .padding(.leading, -16)
+                            .foregroundColor(.black)
                     }
                 }
+
                 
                 Spacer()
                 
@@ -166,7 +198,7 @@ struct ReservationView: View {
             .toolbarBackground(.visible, for: .navigationBar)
             .toolbarRole(.editor)
             .background(Color.backgroundGray)
-
+            
         }
     }
     
